bd12b00a81188a76294608ee57369865f0463ff0
Вшивков Михаил <vshivkov.ma@gmail.com>
2019-06-23 15:06:54 +0500

    Вывод таблицы из json, поиск по проектам

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, PipeTransform } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,10 +8,18 @@ import { HomeComponent } from "./home/home.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HttpClientModule } from "@angular/common/http";
 
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [HttpClientModule, NgbModule, BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    HttpClientModule,
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,60 @@
-import { JsonImporterService } from "./../services/json-importer/json-importer.service";
-import { Component, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+import { Component, PipeTransform, OnInit, OnDestroy } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormControl } from "@angular/forms";
+import { DecimalPipe } from "@angular/common";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.sass"]
+  styleUrls: ["./home.component.sass"],
+  providers: [DecimalPipe]
 })
-export class HomeComponent implements OnInit {
-  constructor(private jsonImporterService: JsonImporterService) {}
+export class HomeComponent implements OnInit, OnDestroy {
+  rawTransactions: Array<any>;
+  filteredTransactions: Observable<any[]>;
+  filter = new FormControl("");
 
-  ngOnInit() {
-    this.jsonImporterService.getJSON().subscribe(data => {
-      console.log(data);
+  private defaultDataPath: string;
+  private transactionsSubscription: Subscription;
+
+  constructor(private http: HttpClient, pipe: DecimalPipe) {
+    this.defaultDataPath = "/assets/data/data.json";
+  }
+
+  ngOnInit(): void {
+    this.loadRawTransactions();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll();
+  }
+
+  private unsubscribeAll() {
+    this.transactionsSubscription.unsubscribe();
+  }
+
+  private loadRawTransactions() {
+    this.transactionsSubscription = this.http
+      .get(this.defaultDataPath)
+      .subscribe(data => {
+        this.rawTransactions = data as any[];
+        this.filterTransactions();
+      });
+  }
+
+  private filterTransactions() {
+    this.filteredTransactions = this.filter.valueChanges.pipe(
+      startWith(""),
+      map(text => this.search(text))
+    );
+  }
+
+  public search(text: string): any[] {
+    return this.rawTransactions.filter(entity => {
+      const term = text.toLowerCase();
+      return entity.transaction.project.name.toLowerCase().includes(term);
     });
   }
 }
