f14acc17895870c0af78fbb2a6ce59a4ef47baf1
Вшивков Михаил <vshivkov.ma@gmail.com>
2019-06-25 17:23:05 +0500

    Вывод рейтинга платёжных систем

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,11 @@ import { DecimalPipe } from "@angular/common";
   providers: [DecimalPipe]
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  rawTransactions: Array<any>;
+  rawTransactions: any[];
+  paymentSystems: any[];
   filteredTransactions: Observable<any[]>;
   filter = new FormControl("");
+  projects: string;
 
   private defaultDataPath: string;
   private transactionsSubscription: Subscription;
@@ -41,7 +43,35 @@ export class HomeComponent implements OnInit, OnDestroy {
       .subscribe(data => {
         this.rawTransactions = data as any[];
         this.filterTransactions();
+        this.parseProjects();
+        this.parsePaymentSystemRating();
       });
+  }
+
+  private parseProjects() {
+    this.projects = Array.from(
+      new Set(this.rawTransactions.map(x => x.transaction.project.name))
+    ).join(", ");
+  }
+
+  private parsePaymentSystemRating() {
+    const uncountedPaymentSystems = this.rawTransactions.map(
+      x => x.transaction.payment_method.name
+    ) as string[];
+
+    const unsortedSystems = {};
+
+    // tslint:disable-next-line: forin
+    for (const index in Object.keys(uncountedPaymentSystems)) {
+      const system = uncountedPaymentSystems[index];
+      unsortedSystems[system] = unsortedSystems[system]
+        ? unsortedSystems[system] + 1
+        : 1;
+    }
+
+    this.paymentSystems = Object.entries(unsortedSystems).sort(
+      (a, b) => (b[1] as number) - (a[1] as number)
+    );
   }
 
   private filterTransactions() {
